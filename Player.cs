b88605b4f0155d9c9f0ae56450d00a25cbd56957
pehilbert <pehilbert03@gmail.com>
2025-10-30 15:27:10 -0700

    Initial commit - Scene setup/player

---
+++ Player.cs
@@ -0,0 +1,35 @@
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+	[Export] public float Speed = 200f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Vector2.Zero;
+
+		// Get input and move
+		if (Input.IsActionPressed("move_right")) 
+		{
+			velocity.X += 1;
+		}
+
+		if (Input.IsActionPressed("move_left")) 
+		{
+			velocity.X -= 1;
+		}
+
+		if (Input.IsActionPressed("move_down")) 
+		{
+			velocity.Y += 1;
+		}
+
+		if (Input.IsActionPressed("move_up")) 
+		{
+			velocity.Y -= 1;
+		}
+
+		Velocity = velocity * Speed;
+		MoveAndSlide();
+	}
+}
